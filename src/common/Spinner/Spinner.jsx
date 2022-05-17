b931eaf1c99eb2cb989d36c59691290ed5e1675e
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-05-17 15:41:13 +0200

    Change spinner component. Load new from react-spinners library.

--- src/common/Spinner/Spinner.jsx
+++ src/common/Spinner/Spinner.jsx
@@ -1,12 +1,9 @@
+import React from 'react';
+import {RingLoader} from "react-spinners";
+
 import './Spinner.css';
 
-export const Spinner = () => <div className="Spinner">
-  <div/>
-  <div/>
-  <div/>
-  <div/>
-  <div/>
-  <div/>
-  <div/>
-  <div/>
-</div>;
+export const Spinner = () => (
+  <div className="Spinner">
+    <RingLoader size={150} color="blueviolet"/>
+  </div>);
