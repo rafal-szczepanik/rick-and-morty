8e101f09f3b29c8a2a3b29858b49b182b47f89ff
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-05-17 15:43:14 +0200

    Provide Not Found page simple styles.

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {GoBackLink} from "../../common/GoBackLink/GoBackLink";
 
+import './NotFound.css'
+
 export const NotFound = () => {
   return (
-    <div>
+    <div className="Not-found">
       <h1>Page not found :(</h1>
       <p>Provide correct page address.</p>
       <GoBackLink/>
