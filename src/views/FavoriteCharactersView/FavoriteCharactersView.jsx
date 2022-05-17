e39d4f5177d33fada8c9675ad3ea97e0c17aeb6f
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-05-17 15:54:46 +0200

    Hide details button when number of elements is lesser than 2.

--- src/views/FavoriteCharactersView/FavoriteCharactersView.jsx
+++ src/views/FavoriteCharactersView/FavoriteCharactersView.jsx
@@ -1,4 +1,5 @@
 import React, {useContext, useState} from 'react';
+import {Link} from "react-router-dom";
 import {CharactersTable} from "../../components/Characters/CharactersTable/CharactersTable";
 import {GlobalContext} from "../../context/GlobalState";
 import {SortButton} from "../../common/SortButton/SortButton";
@@ -7,10 +8,13 @@ import {useWebsiteTitle} from "../../hooks/useWebsiteTitle";
 import '../../components/Characters/CharactersList/CharactersList.css';
 
 export const FavoriteCharactersView = () => {
+
   const {favouriteList} = useContext(GlobalContext);
   useWebsiteTitle('Favourites Characters');
 
   const [isSorted, setIsSorted] = useState(false);
+
+  const elements = favouriteList.map(el => el.id);
 
   const sortedByIdList = [...favouriteList].sort((a, b) => a.id - b.id);
   const sortedByNameList = [...sortedByIdList].sort((a, b) => a.name.localeCompare(b.name));
@@ -20,11 +24,14 @@ export const FavoriteCharactersView = () => {
       <h1 className="Characters-List__heading">Your Favourite Rick and Morty Characters </h1>
       <SortButton isSorted={isSorted} onClick={() => setIsSorted((prevState) => !prevState)}/>
       {sortedByIdList.length === 0
-        ? <h2>No favourites characters in your list.</h2>
+        ? <p>No favourites characters in your list.</p>
         : <CharactersTable
           characters={!isSorted ? sortedByIdList : sortedByNameList}
           actionType="remove"
         />}
+      {elements.length > 1 &&
+        <Link className="Characters-List__link" to={`/favourite-characters/${elements}`}>Get favourite characters
+          details</Link>}
     </div>
   );
 };
