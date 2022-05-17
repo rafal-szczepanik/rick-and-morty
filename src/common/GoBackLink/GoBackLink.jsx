16643f3e114be5cf3e0ee51ed4e7dcd962130f85
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2022-05-17 15:44:58 +0200

    Change styles in GoBack component.

--- src/common/GoBackLink/GoBackLink.jsx
+++ src/common/GoBackLink/GoBackLink.jsx
@@ -3,4 +3,4 @@ import {Link} from "react-router-dom";
 
 import './GoBackLink.css';
 
-export const GoBackLink = () => <Link className="GoBackLink" to="/characters">Go back to all characters</Link>;
+export const GoBackLink = () => <Link className="Go-back-link" to="/characters">Go back to all characters</Link>;
